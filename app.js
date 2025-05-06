960f16ffa96b3b050f0c3eaccd3d5c9ba01d7a89
idukke <idukke@localhost.localdomain>
2025-05-06 20:21:01 +0200

    added public/ and img

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+const USERS_FILE = path.join(__dirname, 'users.json');
+
+
 app.get('/', (req, res) => {
-res.sendFile(path.join(__dirname, 'index.html'));
+res.sendFile(path.join(__dirname, 'public/index.html'));
 });
+
+if (!fs.existsSync(USERS_FILE)) {
+    fs.writeFileSync(USERS_FILE, '[]');
+  }  
+app.post(register, (req, res) => {
+    const {username, email, password} = req.body;
+} )
 
 app.listen(PORT, () => {
 console.log(`Server running at http://localhost:${PORT}`);
 });
+
+/*
+ssh u3002@95.111.234.50
+clave: 3002
+95.111.234.50*/ */
+
+
+//git clone 'url a desplegar'
+//npm install
+
+//contabo.com
